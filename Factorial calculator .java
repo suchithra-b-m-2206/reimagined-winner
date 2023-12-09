d49094471b290007a6fee599eb124cd03d5fb703
Suchithra B M <111634804+suchithra-b-m-2206@users.noreply.github.com>
2023-12-09 23:00:29 +0530

    Factorial calculator .java

---
+++ Factorial calculator .java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.println("Please enter a non-negative integer.");
+        } else {
+            long factorialResult = calculateFactorial(num);
+            System.out.println("Factorial of " + num + " is: " + factorialResult);
+        }
+
+        scanner.close();
+    }
+
+    private static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
